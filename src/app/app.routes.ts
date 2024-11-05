23e6318b832204cefa70c7614b3d0d5cfc611988
Sakkar Icham <89643646+isakkar@users.noreply.github.com>
2024-11-05 19:38:14 +0100

    fixed menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {StudentProfilePageComponent} from './student-profile-page/student-profile-page.component';
+import {AppComponent} from './app.component';
 
 export const routes: Routes = [
   {path:'student-profile-page', component: StudentProfilePageComponent},
--- src/app/student-profile-page/student-profile-page.component.ts
+++ src/app/student-profile-page/student-profile-page.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './student-profile-page.component.html',
   styleUrl: './student-profile-page.component.css'
 })
+
 export class StudentProfilePageComponent {
+  input_surname: any;
+  input_name: any;
 
 }
