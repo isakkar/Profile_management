9e2069f8cd95c940fb94826cbb329a565488b930
Layzerkta <maximquenel1@gmail.com>
2024-11-25 16:00:06 +0100

    Rajout de la page de visualisation de profils

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import {StudentProfilePageComponent} from './student-profile-page/student-profile-page.component';
 import {AppComponent} from './app.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 
 export const routes: Routes = [
   {path:'student-profile-page', component: StudentProfilePageComponent},
+
+  { path: 'view-profile', component: ViewProfileComponent },
+
 ];
