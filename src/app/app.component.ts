4ba5ec91b01a3d923c912043e3e70fc18cf61474
Sakkar Icham <89643646+isakkar@users.noreply.github.com>
2024-12-16 10:31:32 +0100

    Added lessons page and updated home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {StudentProfilePageComponent} from './student-profile-page/student-profile-page.component';
 import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentProfilePageComponent, RouterLink, NgbDropdown, NgbDropdownToggle, NgbDropdownMenu],
+  imports: [RouterOutlet, RouterLink, NgbDropdown, NgbDropdownToggle, NgbDropdownMenu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import {StudentProfilePageComponent} from './student-profile-page/student-profile-page.component';
 import {AppComponent} from './app.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import {ProfileCreationComponent} from './profile-creation/profile-creation.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {ProfileDetailsComponent} from './profile-details/profile-details.component';
-import {PersonalProfileComponent} from './personal-profile/personal-profile.component'; // Import the home page component
+import {PersonalProfileComponent} from './personal-profile/personal-profile.component';
+import {LessonsComponent} from './lessons/lessons.component'; // Import the home page component
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent }, // Default route for the home page
@@ -13,4 +13,5 @@ export const routes: Routes = [
   { path: 'view-profile', component: ViewProfileComponent },
   { path: 'profile-details/:id', component: ProfileDetailsComponent },
   { path: 'personal-profile', component: PersonalProfileComponent },
+  { path: 'lessons', component: LessonsComponent },
 ];
