e9514d443c6c5fb0702b0a0ab3597ddad4d696de
Sakkar Icham <89643646+isakkar@users.noreply.github.com>
2024-12-16 09:12:41 +0100

    Added personal page and revamped home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {AppComponent} from './app.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import {ProfileCreationComponent} from './profile-creation/profile-creation.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import {ProfileDetailsComponent} from './profile-details/profile-details.component'; // Import the home page component
+import {ProfileDetailsComponent} from './profile-details/profile-details.component';
+import {PersonalProfileComponent} from './personal-profile/personal-profile.component'; // Import the home page component
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent }, // Default route for the home page
   {path:'student-profile-page', component: StudentProfilePageComponent},
   {path:'profile-creation', component:ProfileCreationComponent},
   { path: 'view-profile', component: ViewProfileComponent },
-  { path: 'profile-details/:id', component: ProfileDetailsComponent }, // Add this route
+  { path: 'profile-details/:id', component: ProfileDetailsComponent },
+  { path: 'personal-profile', component: PersonalProfileComponent },
 ];
